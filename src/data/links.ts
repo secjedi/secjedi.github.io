2821f592d97ca882ff620e8eddbf9258a175a858
secjedi <mondayevidence@gmail.com>
2025-08-16 22:03:33 +0200

    website overhaul

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,17 +1,17 @@
-import TwitterIcon from '@/components/icons/TwitterIcon'
+import TwitterIcon from '@/components/icons/LinkedinIcon'
 import GithubIcon from '@/components/icons/GithubIcon'
 
 // ADD YOUR SOCIAL NETWORKS HERE
 export const SOCIALNETWORKS = [
 	{
 		name: 'Github',
-		url: 'https://github.com/danielcgilibert/blog-template',
+		url: 'https://github.com/secjedi',
 		icon: GithubIcon
 	},
 
 	{
-		name: 'Twitter',
-		url: 'https://github.com/danielcgilibert/blog-template',
+		name: 'LinkedIn',
+		url: 'https://www.linkedin.com/in/evidencemonday/',
 		icon: TwitterIcon
 	}
 ] as const
--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -1,4 +1,5 @@
 interface SiteConfig {
+	site: string
 	author: string
 	title: string
 	description: string
@@ -9,16 +10,12 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
+	site: 'https://evidencemonday.site', // Write here your website url
 	author: 'Evidence Monday', // Site author
-	title: 'Evidence\'s blog', // Site title.
-	description: 'Learning daily... and documenting it', // Description to display in the meta tags
+	title: "Evidence's blog", // Site title.
+	description: 'Learning daily... and documenting it.', // Description to display in the meta tags
 	lang: 'en-GB',
 	ogLocale: 'en_GB',
 	shareMessage: 'Share this post', // Message to share a post on social media
 	paginationSize: 6 // Number of posts per page
 }
-export const siteName = 'Evidence\'s blog' // Site name for the title tag
-export const siteUrl = 'https://evidencemonday.site' // Site URL for the canonical tag
-export const siteImage = `${siteUrl}/og-image.png` // Site image for the Open Graph tags
-export const siteFavicon = `${siteUrl}/favicon.png` // Site favicon URL
-export const siteLogo = `${siteUrl}/logo.png` // Site logo URL for the header
